d896a36ebe9f4906cd4884809f7b4a496513b148
DevEn <kangingoo31@gmail.com>
2024-10-19 18:36:57 +0900

    24.10.19_css및 start 수정

--- src/components/ChatStart.jsx
+++ src/components/ChatStart.jsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from 'react';
+import './ChatStart.css';
+import React, { useRef, useState } from "react";
 
 function ChatStart() {
   const refRoom = useRef();
@@ -9,30 +10,35 @@ function ChatStart() {
   const openChatWin = () => {
     window.open(
       `#/chat?roomId=${refRoom.current.value}&userId=${refId.current.value}`,
-      '',
-      'width=500, height=500'
+      "",
+      "width=500, height=840"
     );
   };
 
   return (
-    <div className='chatStart'>
-      <h2 className="start_tit">대화명 만들기</h2>
-      채팅방 이름 :{' '}
-      <input
-        type="text"
-        name="roomId"
-        value={roomId}
-        placeholder="채팅할 유저의 아이디"
-        ref={refRoom}
-        onChange={(e) => setRoomId(e.target.value)}
-      />
-      <br />
-      대화명 :{' '}
-      <input type="text" name="userId" value="admin" ref={refId} readOnly />
-      <br />
-      <button type="button" onClick={openChatWin} className="start_btn">
-        채팅방 입장
-      </button>
+    <div className="chatStart">
+      <h2 className="start_tit">채팅 입장 - 관리자</h2>
+      <div className="inner">
+        <div className="input_wrap">
+          <p>채팅방 이름 :{" "}</p>
+          <input
+            type="text"
+            name="roomId"
+            value={roomId}
+            placeholder="채팅할 유저의 아이디"
+            ref={refRoom}
+            onChange={(e) => setRoomId(e.target.value)}
+          />
+        </div>
+
+        <div className="input_wrap">
+          <p>대화명 :{" "}</p>
+          <input type="text" name="userId" value="admin" ref={refId} readOnly />
+        </div>
+        <button type="button" onClick={openChatWin} className="start_btn">
+          채팅방 입장
+        </button>
+      </div>
     </div>
   );
 }
